f9da00d8f6b706776ce0c6c73d31044a49cb9d1e
muhammad.junaid <muhammad.junaid@KARACHI-570.360training.com>
2017-02-24 17:44:42 +0500

    Removed unused code and utilized StringBuilder class instead of StringBuffer.

--- src/java/com/softech/vu360/lms/repositories/TrainingPlanRepositoryCustom.java
+++ src/java/com/softech/vu360/lms/repositories/TrainingPlanRepositoryCustom.java
@@ -3,8 +3,6 @@ package com.softech.vu360.lms.repositories;
 import java.util.List;
 import java.util.Map;
 
-import com.softech.vu360.lms.model.CustomerEntitlement;
-
 public interface TrainingPlanRepositoryCustom {
 	
 	List<Map<Object, Object>> getTrainingPlansForCourseCatalog(long id,String search);
