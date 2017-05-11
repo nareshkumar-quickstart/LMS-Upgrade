dd82e3d335a6c72191f8588f138102430c27fa8b
muhammad.sajjad <muhammad.sajjad@360training.com>
2017-05-11 19:40:46 +0500

    http://jira.360training.com/browse/LMS-22257

--- src/java/com/softech/vu360/lms/web/velocity/AcegiVelocityContextInterceptor.java
+++ src/java/com/softech/vu360/lms/web/velocity/AcegiVelocityContextInterceptor.java
@@ -124,6 +124,8 @@ public class AcegiVelocityContextInterceptor extends HandlerInterceptorAdapter {
 			ModelAndView modelAndView) throws Exception {
 		log.debug("Inside AcegiVelocityContextInterceptor.postHandle()");
 		HttpSession menuSession = request.getSession(true);
+		//Sajjad - initedClustered was not getting deleted from session when user goto login page without logout
+		// This causes user's currentMode set to null  - LMS-22257
 		if(request.getRequestURI().indexOf("login.do") > -1){
 			request.getSession().setAttribute("initedClustered", null);
 		}
